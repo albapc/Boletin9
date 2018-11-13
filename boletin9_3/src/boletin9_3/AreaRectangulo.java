21cc10be0d88d80acaa27a547ad192aade9b5791
albapc <alba.perez.ce@gmail.com>
2018-11-13 23:06:07 +0100

    terminar

--- boletin9_3/src/boletin9_3/AreaRectangulo.java
+++ boletin9_3/src/boletin9_3/AreaRectangulo.java
@@ -1,19 +1,19 @@
-
 package boletin9_3;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class AreaRectangulo {
+
     private double base, altura;
-    
-    public void calcularArea(){
-        Scanner sc= new Scanner(System.in);
-        base=sc.nextDouble();
-        
-//        System.out.println("Introduce a base");
-//        
-//        System.out.println("Introduce a altura");
-//        altura=sc.nextDouble();
-//        System.out.println("Area rectángulo: "+base*altura);
+
+    public void calcularArea() {
+        do {
+            base = Double.parseDouble(JOptionPane.showInputDialog("Introduce a base:"));
+        } while (base <= 0);
+        do {
+            altura = Double.parseDouble(JOptionPane.showInputDialog("Introduce a altura:"));
+        } while (altura <= 0);
+
+        System.out.println("A área do rectángulo é: " + base * altura);
     }
 }
--- boletin9_3/src/boletin9_3/Boletin9_3.java
+++ boletin9_3/src/boletin9_3/Boletin9_3.java
@@ -1,10 +1,11 @@
-
 package boletin9_3;
 
 public class Boletin9_3 {
 
     public static void main(String[] args) {
-       
+        AreaRectangulo obxArea = new AreaRectangulo();
+
+        obxArea.calcularArea();
     }
-    
+
 }
