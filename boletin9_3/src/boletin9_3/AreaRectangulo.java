84f0c58db916cba92dcc0f65aeafa092744f40b6
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2018-11-13 13:18:38 +0100

    en proceso

---
+++ boletin9_3/src/boletin9_3/AreaRectangulo.java
@@ -0,0 +1,19 @@
+
+package boletin9_3;
+
+import java.util.Scanner;
+
+public class AreaRectangulo {
+    private double base, altura;
+    
+    public void calcularArea(){
+        Scanner sc= new Scanner(System.in);
+        base=sc.nextDouble();
+        
+//        System.out.println("Introduce a base");
+//        
+//        System.out.println("Introduce a altura");
+//        altura=sc.nextDouble();
+//        System.out.println("Area rectángulo: "+base*altura);
+    }
+}
